f881c165d9771920520920f029db559b610aaef5
Robert Soriano <sorianorobertc@gmail.com>
2021-09-23 14:44:44 +0800

    update vite config

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
 import { useMotions } from '../lib'
 
 const motions = useMotions()
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,44 @@
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+const resolvePath = (str) => resolve(__dirname, str)
+const isProd = process.env.NODE_ENV === "production"
+
+const devConfig = defineConfig({
+  plugins: [vue()],
+  build: {
+    outDir: 'dist-demo'
+  },
 })
+
+const prodConfig = defineConfig({
+  build: {
+    lib: {
+      entry: resolvePath("lib/index.ts"),
+      name: 'v-motion',
+      fileName: (format) => `v-motion.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue', 'motion'],
+      output: {
+        globals: {
+          vue: 'Vue',
+          motion: 'motion',
+        },
+      },
+    },
+  },
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+      compilerOptions: {
+        noEmit: false,
+        declaration: true
+      }
+    })
+  ]
+})
+
+export default isProd ? prodConfig : devConfig
